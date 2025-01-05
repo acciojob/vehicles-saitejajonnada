c4780f77882ecdf59ab7e94c8ca7d865d36ef877
Jonnada Sai Teja <146433770+saitejajonnada@users.noreply.github.com>
2025-01-05 18:07:39 +0530

    Update WaterVehicle.java

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -36,11 +36,3 @@ public class Boat implements WaterVehicle
     }
 }
 
-public static void main(String[] args) {
-    Boat boat = new Boat("Sea Explorer", 20);
-
-    System.out.println("Boat Name: " + boat.getVehicleName());
-    System.out.println("Boat Capacity: " + boat.getVehicleCapacity());
-    
-}
-
